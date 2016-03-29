32e1e93d0679e20d4c6131f8fb0795d05bddc709
Alex Manzella <alex.manzella@runtastic.com>
2016-03-29 19:44:30 +0200

    Update project and add test dependencies

---
+++ KakapoExample/KakapoTests/KakapoTests.swift
@@ -0,0 +1,27 @@
+//
+//  KakapoTests.swift
+//  KakapoTests
+//
+//  Created by Alex Manzella on 29/03/16.
+//  Copyright © 2016 devlucky. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import Kakapo
+
+class TableOfContentsSpec: QuickSpec {
+    override func spec() {
+        describe("Not implemented") {
+            it("has nothing") {
+                expect(false).to(beTrue())
+            }
+            
+            context("if it doesn't have what you're looking for") {
+                it("needs to be updated") {
+                    expect(false).to(beTrue())
+                }
+            }
+        }
+    }
+}
