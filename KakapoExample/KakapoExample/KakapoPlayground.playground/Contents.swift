368d78c51226f3b2cbbeede5c7ad7d7be614e620
Alex Manzella <manzopower@icloud.com>
2016-04-28 13:44:03 +0200

    ADD some tests for JSON API serialization

--- KakapoExample/KakapoExample/KakapoPlayground.playground/Contents.swift
+++ KakapoExample/KakapoExample/KakapoPlayground.playground/Contents.swift
@@ -2,3 +2,29 @@
 
 import UIKit
 @testable import Kakapo
+
+struct Dog: JSONAPIEntity {
+    let id: Int
+    let name: String
+}
+
+struct Friend: JSONAPIEntity {
+    let id: Int
+    let name: String
+    let dog: Dog
+}
+
+struct User: JSONAPIEntity {
+    let id: Int
+    let name: String
+    let friend: [Friend]
+}
+
+func pprint(obj: AnyObject) {
+    let data = try! NSJSONSerialization.dataWithJSONObject(obj, options: .PrettyPrinted)
+    print(NSString(data: data, encoding: 0)!)
+}
+
+pprint(JSONAPISerializer(User(id: 1, name: "s", friend: [Friend(id: 33, name: "a", dog: Dog(id: 134, name: "Zarco"))])).serialize())
+
+//pprint(JSONAPISerializer([Friend(id: 33, name: "a")]).serialize())
--- KakapoExample/KakapoTests/JSONAPITests.swift
+++ KakapoExample/KakapoTests/JSONAPITests.swift
@@ -9,24 +9,104 @@
 import Foundation
 import Quick
 import Nimble
+import SwiftyJSON
 @testable import Kakapo
 
 class JSONAPISpec: QuickSpec {
     
-    struct User: JSONAPIEntity {
+    struct Dog: JSONAPIEntity {
+        let id: Int
         let name: String
     }
     
+    struct User: JSONAPIEntity {
+        let id: Int
+        let name: String
+        let dog: Dog
+    }
+    
+    struct Post: JSONAPIEntity {
+        let id: Int
+        let relatedPostIds: [Int]
+    }
+    
     override func spec() {
-        let user = User(name: "Alex")
         
-        describe("JSON API serialization") {
-            it("....") {
-                let serialized = JSONAPISerializer(user)
+        let user = User(id: 11, name: "Alex", dog: Dog(id: 22, name: "Joan"))
+
+        func json(object: Serializable) -> JSON {
+            return JSON(object.serialize())
+        }
+        
+        describe("JSON API Serialzier") {
+            it("should serialize data") {
+                let object = json(JSONAPISerializer(user))
+                let data = object["data"].dictionaryValue
+                expect(data.count).toNot(equal(0))
+            }
+        }
+        
+        describe("JSON API Entity Serialization") {
+            it("should serialize the attributes") {
+                let object = json(user)
+                let attributes = object["attributes"]
+                expect(attributes["id"].stringValue).to(beNil())
+                expect(attributes["name"].stringValue).to(equal("Alex"))
+                expect(attributes.dictionaryValue.count).to(equal(1))
             }
             
-            it("......") {
-                let serialized = JSONAPISerializer([user])
+            it("should serialzie the id") {
+                let object = json(user)
+                expect(object["id"].stringValue).to(equal("11"))
+            }
+            
+            it("should serialzie the type") {
+                let object = json(user)
+                expect(object["type"].stringValue).to(equal("user"))
+            }
+            
+            it("should serialzie an arrays of non-JSONAPIEntities as an attribute") {
+                let object = json(Post(id: 11, relatedPostIds: [1, 2, 3]))
+                let relatedPostIds = object["attributes"]["relatedPostIds"].arrayValue
+                expect(relatedPostIds.count).to(equal(3))
+                expect(relatedPostIds[0]).to(equal(1))
+                expect(relatedPostIds[1]).to(equal(2))
+                expect(relatedPostIds[2]).to(equal(3))
+            }
+            
+            it("should serialzie an array of JSONAPIEntities") {
+                let objects = json([user, user]).arrayValue
+                for object in objects {
+                    expect(object["attributes"]["name"].stringValue).to(equal("Alex"))
+                }
+                
+                expect(objects.count).to(equal(2))
+            }
+        }
+        
+        describe("JSON API Entity with PropertyPolicies") {
+            // TODO: ....
+        }
+        
+        describe("JSON API  Entity relationship serialization") {
+            it("should serialzie the relationships when they are single JSONAPIEntities") {
+                
+            }
+            
+            it("should serialzie the relationships when they are arrays of JSONAPIEntities") {
+                
+            }
+            
+            it("should not serialzie relationships of relationships") {
+                
+            }
+            
+            it("should serialzie the relationships even when an array is empty") {
+                
+            }
+            
+            it("should not serialzie nil relationships") {
+                
             }
         }
     }
