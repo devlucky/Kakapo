d56cf2b98dc5b727f53c555fa2ba65282bb4cef3
Alex Manzella <manzopower@icloud.com>
2016-04-28 12:44:14 +0200

    ADD Array JSON serialization

--- Source/JSONAPISerialization.swift
+++ Source/JSONAPISerialization.swift
@@ -8,43 +8,79 @@
 
 import Foundation
 
-protocol JSONAPIConvertible {
-    func data() -> [String: AnyObject]
+public protocol JSONAPISerializable {
+    func data(includeRelationships includeRelationships: Bool, includeAttributes: Bool) -> AnyObject?
 }
 
-protocol JSONAPIEntity: Serializable, JSONAPIConvertible {
+public protocol JSONAPIEntity: CustomSerializable, JSONAPISerializable {
     var type: String { get }
     var id: Int { get }
 }
 
-extension JSONAPIEntity {
+extension Array: JSONAPISerializable {
+    public func data(includeRelationships includeRelationships: Bool, includeAttributes: Bool) -> AnyObject? {
+        let data = flatMap { ($0 as? JSONAPISerializable)?.data(includeRelationships: includeRelationships, includeAttributes: includeAttributes) }
+        return data.count > 0 ? data : nil
+    }
+}
+
+public extension JSONAPIEntity {
     var type: String {
-        return String(self).lowercaseString
+        return String(self.dynamicType).lowercaseString
     }
-}
-
-extension Array where Element: JSONAPIEntity {
-    func data() -> [String: AnyObject] {
-        return [String: AnyObject]()
+    
+    public func customSerialize() -> AnyObject {
+        return data(includeRelationships: true, includeAttributes: true)!
     }
-}
+    
+    public func data(includeRelationships includeRelationships: Bool, includeAttributes: Bool) -> AnyObject? {
+        var data = [String: AnyObject]()
+        
+        data["id"] = String(id)
+        data["type"] = type
+        
+        let mirror = Mirror(reflecting: self)
+        
+        var attributes = [String: AnyObject]()
+        var relationships = [String: AnyObject]()
+        
+        for child in mirror.children {
+            if let label = child.label {
+                if child.value is _PropertyPolicy {
+                    
+                } else if let value = child.value as? JSONAPISerializable, let data = value.data(includeRelationships: true, includeAttributes: false) {
+                    relationships[label] =  ["data": data]
+                } else if let value = child.value as? Serializable {
+                    attributes[label] = value.serialize()
+                } else {
+                    assert(child.value is AnyObject)
+                    attributes[label] = child.value as? AnyObject
+                }
+            }
+        }
+        
+        if includeAttributes && attributes.count > 0 {
+            data["attributes"] = attributes
+        }
+        
+        if includeRelationships && relationships.count > 0 {
+            data["relationships"] = relationships
+        }
 
-extension JSONAPIConvertible {
-    func data() -> [String: AnyObject] {
-        return [String: AnyObject]()
+        return data
     }
 }
 
 struct JSONAPISerializer<T: JSONAPIEntity>: CustomSerializable {
     
-    let data: [String: AnyObject]
+    let data: AnyObject
     
     init(_ object: T) {
-        data = object.data()
+        data = object.serialize()
     }
     
     init(_ objects: [T]) {
-        data = objects.data()
+        data = objects.serialize()
     }
     
     func customSerialize() -> AnyObject {
