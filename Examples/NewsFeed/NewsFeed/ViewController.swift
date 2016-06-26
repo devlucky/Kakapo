0355f8179958c0b73750d6269baa832c8a983cee
Alex Manzella <manzopower@icloud.com>
2016-06-26 19:25:27 +0200

    ADD NewsFeed sample

---
+++ Examples/NewsFeed/NewsFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Alex Manzella on 26/06/16.
+//  Copyright © 2016 devlucky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Examples/NewsFeed/Pods/Target Support Files/Kakapo/Kakapo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double KakapoVersionNumber;
+FOUNDATION_EXPORT const unsigned char KakapoVersionString[];
+
