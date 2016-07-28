24fb2bc066fcc679e5a25cfc12e836ef423388be
Joan Romano <joanromano@gmail.com>
2016-07-28 14:12:37 +0200

    Display comments on cells

--- Examples/NewsFeed/NewsFeed/PostTableViewCell.swift
+++ Examples/NewsFeed/NewsFeed/PostTableViewCell.swift
@@ -15,6 +15,8 @@ class PostTableViewCell: UITableViewCell {
     private let avatarImage = UIImageView()
     private let likeButton = UIButton()
     private let likeCountLabel = UILabel()
+    private let commentButton = UIButton()
+    private let commentCountLabel = UILabel()
     
     private var likeHandler: (() -> ())?
     
@@ -33,23 +35,26 @@ class PostTableViewCell: UITableViewCell {
         postLabel.text = post.text
         likeCountLabel.text = "\(post.likes.count)"
         likeButton.tintColor = post.isLikedByMe ? .blueColor() : .grayColor()
+        commentCountLabel.text = "\(post.comments.count)"
         self.likeHandler = likeHandler
     }
     
     private func styleUI() {
+        commentButton.setImage(UIImage(named: "comment")?.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
+        commentButton.tintColor = .grayColor()
         likeButton.setImage(UIImage(named: "thumbUp")?.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
-        likeButton.setTitleColor(.blueColor(), forState: .Normal)
         likeButton.addTarget(self, action: #selector(likeButtonPressed), forControlEvents: .TouchUpInside)
         backgroundColor = UIColor.whiteColor()
         postLabel.numberOfLines = 0
     }
     
     private func layoutUI() {
-        ([authorLabel, postLabel, avatarImage, likeButton, likeCountLabel] as [UIView]).forEach { (view) in
+        ([authorLabel, postLabel, avatarImage, likeButton, likeCountLabel, commentButton, commentCountLabel] as [UIView]).forEach { (view) in
             addSubview(view)
         }
         
         let margin = 10
+        let biggerMargin = 20
 
         authorLabel.snp_makeConstraints { (make) in
             make.top.leading.equalTo(self).offset(margin)
@@ -62,7 +67,7 @@ class PostTableViewCell: UITableViewCell {
         }
         
         likeButton.snp_makeConstraints { (make) in
-            make.top.equalTo(postLabel.snp_bottom).offset(margin)
+            make.top.equalTo(postLabel.snp_bottom).offset(biggerMargin)
             make.leading.equalTo(self).offset(margin)
             make.bottom.equalTo(self).inset(margin)
         }
@@ -70,6 +75,16 @@ class PostTableViewCell: UITableViewCell {
         likeCountLabel.snp_makeConstraints { (make) in
             make.top.bottom.equalTo(likeButton)
             make.leading.equalTo(likeButton.snp_trailing).offset(margin)
+        }
+        
+        commentButton.snp_makeConstraints { (make) in
+            make.top.bottom.equalTo(likeButton)
+            make.leading.equalTo(likeCountLabel.snp_trailing).offset(biggerMargin)
+        }
+        
+        commentCountLabel.snp_makeConstraints { (make) in
+            make.top.bottom.equalTo(likeButton)
+            make.leading.equalTo(commentButton.snp_trailing).offset(margin)
         }
     }
     
